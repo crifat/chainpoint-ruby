a86328fd6d5e7472df153b26cad2e89ffc4c9e7c
Sajib Hassan <sajib.hassan@gmail.com>
2020-08-19 01:17:44 +0600

    the proofs handler tasks with initial test coverage

--- lib/chainpoint/get_proof.rb
+++ lib/chainpoint/get_proof.rb
@@ -1,8 +1,8 @@
 module Chainpoint
   class GetProof
-    include Chainpoint::Utils::Helpers
-    include Chainpoint::Utils::Network
-    include Chainpoint::Utils::Proofs
+    include ::Chainpoint::Utils::Helpers
+    include ::Chainpoint::Utils::Network
+    include ::Chainpoint::Utils::Proofs
 
     def initialize(proof_handlers = [])
       @proof_handlers = proof_handlers || []
@@ -28,10 +28,10 @@ module Chainpoint
 
         gateways_with_get_opts_data = gateways_with_get_opts(uuids_by_gateway)
 
-        fetch_results = fetch_endpoints(gateways_with_get_opts_data)
-
+        fetch_endpoints(gateways_with_get_opts_data)
       rescue => e
-
+        puts e.message
+        raise ::Chainpoint::Error, e.message
       end
 
     end
@@ -39,19 +39,13 @@ module Chainpoint
     def fetch_endpoints(gateway_uris_opts)
       results = []
       gateway_uris_opts.each do |options|
-        # options  = gateway_uri_options(geteway_uri, hashes)
         begin
           response = submit_data(options)
-          raise Chainpoint::Error, response.message if response.code.to_i >= 400
-          result                         = JSON.parse(response.read_body)
-          result["meta"]["submitted_to"] = options[:base_uri]
-          results << {
-              uri:      options[:base_uri],
-              response: result,
-              error:    nil
-          }
+          raise ::Chainpoint::Error, response.message if response.code.to_i >= 400
+          result                     = JSON.parse(response.read_body)
+          result["anchors_complete"] ||= []
+          results << result
         rescue => e
-
           puts e.message
         end
       end
@@ -81,58 +75,18 @@ module Chainpoint
     def validate_proof_handlers
       # Validate all proofHandles provided
       test_array_args(@proof_handlers)
-      raise Chainpoint::Error, 'proofHandles Array contains invalid Objects' if @proof_handlers.any? { |handler| !is_valid_proof_handle(handler) }
-      raise Chainpoint::Error, 'proofHandles arg must be an Array with <= 250 elements' if @proof_handlers.size > 250
+      raise ::Chainpoint::Error, 'proofHandles Array contains invalid Objects' if @proof_handlers.any? { |handler| !is_valid_proof_handle(handler) }
+      raise ::Chainpoint::Error, 'proofHandles arg must be an Array with <= 250 elements' if @proof_handlers.size > 250
 
       # Validate that *all* URI's provided are valid or throw
       bad_handle_uris = @proof_handlers.reject { |ph| is_valid_uri?(ph['uri']) }
 
-      raise Chainpoint::Error, "some proof handles contain invalid URI values : #{bad_handle_uris.map { |h| h['uri'] }.join(', ')}" unless bad_handle_uris.empty?
+      raise ::Chainpoint::Error, "some proof handles contain invalid URI values : #{bad_handle_uris.map { |h| h['uri'] }.join(', ')}" unless bad_handle_uris.empty?
 
       # Validate that *all* proofIds provided are valid or throw
       bad_handle_uuids = @proof_handlers.reject { |ph| is_valid_uuid?(ph['proof_id']) }
-      raise Chainpoint::Error, "some proof handles contain invalid proofId UUID values : #{bad_handle_uuids.map { |h| h['proof_id'] }.join(', ')}" unless bad_handle_uuids.empty?
+      raise ::Chainpoint::Error, "some proof handles contain invalid proof_id UUID values : #{bad_handle_uuids.map { |h| h['proof_id'] }.join(', ')}" unless bad_handle_uuids.empty?
     end
 
   end
 end
-
-
-#
-#   try {
-#
-#     // For each Gateway construct a set of GET options including
-#     // the `proofIds` header with a list of all hash ID's to retrieve
-#     // proofs for from that Gateway.
-
-#
-#     let fetchResults = await fetchEndpoints(gatewaysWithGetOpts)
-#
-#     let getResponses = fetchResults
-#       // Filter out and log any failed requests
-#       .filter(result => {
-#         if (result.error) {
-#           console.log(`Client error getting proof from ${result.uri} : ${result.error} : Skipping`)
-#           return false
-#         }
-#         return true
-#       })
-#       // Return an array of response bodies
-#       .map(result => result.response)
-#
-#     let flatParsedBody = flatten(getResponses)
-#
-#     let proofsResponse = []
-#
-#     forEach(flatParsedBody, proofResp => {
-#       // Set to empty Array if unset of null
-#       proofResp.anchors_complete = proofResp.anchors_complete || []
-#       // Camel case object keys
-#       let proofRespCamel = mapKeys(proofResp, (v, k) => camelCase(k))
-#       proofsResponse.push(proofRespCamel)
-#     })
-#     return proofsResponse
-#   } catch (err) {
-#     console.error(err.message)
-#     throw err
-#   }
---
+++ test/chainpoint/get_proof_test.rb
@@ -0,0 +1,50 @@
+require "#{File.dirname(__FILE__)}/test_helper.rb"
+
+class Chainpoint::GetProofTest < MiniTest::Test
+  def setup
+    json_file           = File.open File.join(File.dirname(__FILE__), 'data/proof-handles.json')
+    @proof_handles_data = JSON.load(json_file)
+    json_file.close
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test_should_only_accept_an_array_of_valid_proof_handles
+    error, message = get_proof([])
+    assert(error && message.include?('non-empty Array'), 'Should have thrown with an empty array')
+
+    error, message = get_proof('not an array')
+    assert(error && message.include?('an Array'), 'Should have thrown with a non-array arg')
+
+    large_data_set = [].fill(@proof_handles_data.first, 0, 255)
+    _, message     = get_proof(large_data_set)
+    assert(message.include?('<= 250'), 'Should have thrown with a data set larger than 250 items')
+
+    error, message = get_proof([@proof_handles_data.first.merge({ "uri" => 'invalid-uri' })])
+    assert(error && message.include?('invalid URI values'), 'Should have thrown with an invalid uri')
+
+    error, message = get_proof([{ foo: 'bar' }])
+    assert(error && message.include?('invalid Objects'), 'Should have thrown with an invalid handle')
+
+    error, message = get_proof([@proof_handles_data.first.merge({ "proof_id" => '123456' })])
+    assert(error && message.include?('invalid proof_id'), 'Should have thrown with an invalid proof_id')
+
+  end
+
+  private
+
+  def get_proof(phs)
+    error         = false
+    error_message = ""
+    begin
+      ::Chainpoint::GetProof.new(phs).perform
+    rescue => e
+      error         = true
+      error_message = e.message
+    end
+
+    [error, error_message]
+  end
+end
